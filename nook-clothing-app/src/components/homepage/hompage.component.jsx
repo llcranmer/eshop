63606b38e78809099775b786c074375ecf7d53cf
llc <touldeguiacranmer@gmail.com>
2020-04-30 19:36:47 -0500

    basic layout lesson 60

--- nook-clothing-app/src/components/homepage/hompage.component.jsx
+++ nook-clothing-app/src/components/homepage/hompage.component.jsx
@@ -1,7 +1,40 @@
 import React from 'react';
-
+import "./homepage.styles.scss";
 const HomePage = () => (
-    <div>Test</div>
+    <div className="homepage">
+        <div className="directory-menu">
+            <div className="directory-menu-item">
+                <div className="content-box">
+                    <h1 className="title"> Mens</h1>
+                    <span className="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+            <div className="directory-menu-item">
+                <div className="content-box">
+                    <h1 className="title"> WOMENS</h1>
+                    <span className="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+            <div className="directory-menu-item">
+                <div className="content-box">
+                    <h1 className="title">JACKETS</h1>
+                    <span className="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+            <div className="directory-menu-item">
+                <div className="content-box">
+                    <h1 className="title">HATS</h1>
+                    <span className="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+            <div className="directory-menu-item">
+                <div className="content-box">
+                    <h1 className="title">SHOES</h1>
+                    <span className="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+        </div>
+    </div>
 )
 
 
