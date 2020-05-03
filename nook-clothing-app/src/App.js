e7f602d8b5158c6482fed352931eb367c812391d
llc <touldeguiacranmer@gmail.com>
2020-05-03 18:38:10 -0500

    sign up

--- nook-clothing-app/src/App.js
+++ nook-clothing-app/src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 import Header from './components/header/header.component';
 import HomePage  from './pages/homepage/hompage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import './App.css';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exapct path='/shop' component={ShopPage} />
+        <Route exact path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
