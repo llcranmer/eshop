622d010c4b81a31aa4cd107eec678cdc5fd61c28
llc <touldeguiacranmer@gmail.com>
2020-05-03 22:28:57 -0500

    sign in w/google

--- nook-clothing-app/src/components/custom-button/custom-button.component.jsx
+++ nook-clothing-app/src/components/custom-button/custom-button.component.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './custom-button.styles.scss';
 
-const CustomButon = ({ children, ...otherProps}) => (
-    <button className='custom-button' {...otherProps}>
+const CustomButton = ({ children, isGoogleSignIn, ...otherProps}) => (
+    <button 
+        className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} {...otherProps}>
         {children}
     </button>
 )
 
-export default CustomButon;
+export default CustomButton;
