701fec38f3f9c5b04fb9fdb52eca3364c720d01c
llc <touldeguiacranmer@gmail.com>
2020-05-04 07:18:40 -0500

    storing user in db

--- nook-clothing-app/src/App.js
+++ nook-clothing-app/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import Header from './components/header/header.component';
 import HomePage  from './pages/homepage/hompage.component';
 import ShopPage from './pages/shop/shop.component';
@@ -18,10 +18,9 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user })
-      console.log('user: ', user)
-    })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
+    createUserProfileDocument(user);
+    });
   }
 
   componentWillUnmount() {
--- nook-clothing-app/src/firebase/firebase.utils.js
+++ nook-clothing-app/src/firebase/firebase.utils.js
@@ -14,6 +14,28 @@ const config = {
     measurementId: "G-ZD7SCN8FLX"
 };
 
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if(!userAuth) return;
+    const userRef = firestore.doc(`users/${userAuth.uid}`)
+    const snapshot = await userRef.get();
+    console.log('snapshot:', snapshot)
+    if(!snapshot.exists) {
+        const { displayName, email} = userAuth;
+        const createdAt = new Date();
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        } catch(e) {
+            console.log('Error creating user', e)
+        }
+    }
+    return userRef;
+}
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
