bbc083ceb778d0aa0ce1fee7b2549c85e4937f4f
llc <touldeguiacranmer@gmail.com>
2020-05-05 22:02:35 -0500

    sign in works

--- nook-clothing-app/src/components/sign-up/sign-up.component.jsx
+++ nook-clothing-app/src/components/sign-up/sign-up.component.jsx
@@ -3,7 +3,6 @@ import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 import { auth, createUserProfileDocument } from '../../firebase/firebase.utils';
 import './sign-up.styles.scss';
-import { render } from '@testing-library/react';
 
 class SignUp extends React.Component {
     constructor(){
