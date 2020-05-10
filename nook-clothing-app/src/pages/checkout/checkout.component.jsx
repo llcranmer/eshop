4f6e38e790c5fe61cf7ade64ae7a3333e9660d35
llc <touldeguiacranmer@gmail.com>
2020-05-09 22:00:52 -0500

    cart items

--- nook-clothing-app/src/pages/checkout/checkout.component.jsx
+++ nook-clothing-app/src/pages/checkout/checkout.component.jsx
@@ -26,9 +26,8 @@ const CheckoutPage = ({ cartItems, total }) => (
         </div>
         {
             cartItems.map(cartItem =>
-             (<CheckoutItem key={cartItem.id} cartItem={cartItem}/>
-            
-        ))}
+             (<CheckoutItem key={cartItem.id} cartItem={cartItem}/>))
+        }
         <div className='total'>
             <span>TOTAL: ${total}</span>
         </div>
